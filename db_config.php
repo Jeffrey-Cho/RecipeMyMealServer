248e9fc2f3112c1e64992c3abd1e4e847fbf107f
Jeffrey <Jeffrey-Cho@users.noreply.github.com>
2015-08-07 21:20:33 +0900

    database config files with connection information

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "bc4485803e7bfc"); // db user
+define('DB_PASSWORD', "5945340e"); // db password (mention your db password here)
+define('DB_DATABASE', "tripled2mysqlgroupdb"); // database name
+define('DB_SERVER', "ap-cdbr-azure-east-c.cloudapp.net"); // db server
+?>
